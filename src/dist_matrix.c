bfc28d610788ed2fba6d4fb238bd79ee7dde4e16
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2018-03-16 21:35:55 +0100

    Enabled Link Time Optimizations and moved static inline functions in dist_matrix.h to dist_matrix.c

--- src/dist_matrix.c
+++ src/dist_matrix.c
@@ -34,6 +34,23 @@ void dist_matrix_free(dist_matrix *dmat) {
     free(dmat);
 }
 
+double *dist_matrix_element(dist_matrix *dmat, uint32_t c1, uint32_t c2) {
+    assert(c1 != c2);
+    assert(c1 >= 0 && c1 < dmat->species_count);
+    assert(c2 >= 0 && c2 < dmat->species_count);
+
+    if (c1 < c2) {
+        return dist_matrix_element(dmat, c2, c1);
+    } else {
+        uint32_t idx = (c1 * (c1 - 1) / 2) + c2;
+        return dmat->distances + idx;
+    }
+}
+
+uint32_t dist_matrix_size(uint32_t species_count) {
+    return species_count * (species_count - 1) / 2;
+}
+
 void dist_matrix_print(const dist_matrix *dmat) {
 
     size_t max_length = 6;
--- src/dist_matrix.h
+++ src/dist_matrix.h
@@ -21,23 +21,10 @@ dist_matrix *dist_matrix_init(uint32_t species_count);
 void dist_matrix_free(dist_matrix *dmat);
 
 /* Extract the distance between species c1 and c2 */
-static double *dist_matrix_element(dist_matrix *dmat, uint32_t c1, uint32_t c2) {
-    assert(c1 != c2);
-    assert(c1 >= 0 && c1 < dmat->species_count);
-    assert(c2 >= 0 && c2 < dmat->species_count);
-
-    if (c1 < c2) {
-        return dist_matrix_element(dmat, c2, c1);
-    } else {
-        uint32_t idx = (c1 * (c1 - 1) / 2) + c2;
-        return dmat->distances + idx;
-    }
-}
+double *dist_matrix_element(dist_matrix *dmat, uint32_t c1, uint32_t c2);
 
 /* Get the matrix size for a given number of species */
-static uint32_t dist_matrix_size(uint32_t species_count) {
-    return species_count * (species_count - 1) / 2;
-}
+uint32_t dist_matrix_size(uint32_t species_count);
 
 /* Print the distance matrix */
 void dist_matrix_print(const dist_matrix *dmat);
