9411aa6d889446c3b54cb0d30ebe3af8026695ab
Elia Cereda <eliacereda@gmail.com>
2018-03-16 12:19:35 +0100

    Added function trim_trailing_space

--- src/io.c
+++ src/io.c
@@ -1,5 +1,19 @@
 #include "io.h"
 #include <stdio.h>
+#include <memory.h>
+#include <ctype.h>
+
+size_t trim_trailing_space(char *s) {
+    size_t length = strlen(s);
+
+    while (length > 0 && isspace(s[length - 1])) {
+        length--;
+    }
+
+    s[length] = '\0';
+
+    return length;
+}
 
 dist_matrix *load_file(const char *file_name) {
     FILE *f = fopen(file_name, "r");
