eb2b5b76edca45e55b20aeb46de4582e369b955e
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2018-03-18 12:05:31 +0100

    Renamed parameters (species) of dist_matrix_element

--- src/dist_matrix.c
+++ src/dist_matrix.c
@@ -39,15 +39,15 @@ void dist_matrix_free(dist_matrix *dmat) {
     }
 }
 
-double *dist_matrix_element(dist_matrix *dmat, uint32_t c1, uint32_t c2) {
-    assert(c1 != c2);
-    assert(c1 >= 0 && c1 < dmat->species_count);
-    assert(c2 >= 0 && c2 < dmat->species_count);
+double *dist_matrix_element(dist_matrix *dmat, uint32_t s1, uint32_t s2) {
+    assert(s1 != s2);
+    assert(s1 >= 0 && s1 < dmat->species_count);
+    assert(s2 >= 0 && s2 < dmat->species_count);
 
-    if (c1 < c2) {
-        return dist_matrix_element(dmat, c2, c1);
+    if (s1 < s2) {
+        return dist_matrix_element(dmat, s2, s1);
     } else {
-        uint32_t idx = (c1 * (c1 - 1) / 2) + c2;
+        uint32_t idx = (s1 * (s1 - 1) / 2) + s2;
         return dmat->distances + idx;
     }
 }
--- src/dist_matrix.h
+++ src/dist_matrix.h
@@ -20,8 +20,8 @@ typedef struct dist_matrix dist_matrix;
 dist_matrix *dist_matrix_init(uint32_t species_count);
 void dist_matrix_free(dist_matrix *dmat);
 
-/* Extract the distance between species c1 and c2 */
-double *dist_matrix_element(dist_matrix *dmat, uint32_t c1, uint32_t c2);
+/* Extract the distance between species s1 and s2 */
+double *dist_matrix_element(dist_matrix *dmat, uint32_t s1, uint32_t s2);
 
 /* Get the matrix size for a given number of species */
 uint32_t dist_matrix_size(uint32_t species_count);
