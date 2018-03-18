e687686541668ed4a975e58528f3212bc86bc63f
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2018-03-18 22:22:13 +0100

    Calculated the distances for the new cluster

--- src/neighbour_joining.c
+++ src/neighbour_joining.c
@@ -40,6 +40,8 @@ dist_matrix *nj_join_nearest_clusters(const dist_matrix *dmat) {
 
     dist_matrix *out = dist_matrix_init(dmat->species_count - 1);
 
+    // FIXME: check out NULL
+    
     uint32_t k = 0;
 
     for (uint32_t i = 0; i < dmat->species_count; i++) {
@@ -52,6 +54,22 @@ dist_matrix *nj_join_nearest_clusters(const dist_matrix *dmat) {
             cluster_size = dmat->cluster_sizes[c1] + dmat->cluster_sizes[c2];
 
             // TODO: compute distances
+            uint32_t l = 0;
+            
+            for (uint32_t j = 0; j < dmat->species_count; j++) {
+                if (j == c2) {
+                    continue;
+                }
+                if (j != c1) {
+                    double d1 = dist_matrix_distance(dmat, c1, j) / cluster_size;
+                    double d2 = dist_matrix_distance(dmat, c2, j) / cluster_size;
+                    
+                    *(dist_matrix_element(out, k, l)) = d1 + d2;
+                }
+                
+                l++;
+            }
+            
         } else if (i == c2) {
             /* Remove cluster c2 */
             continue;
@@ -67,7 +85,7 @@ dist_matrix *nj_join_nearest_clusters(const dist_matrix *dmat) {
                     continue;
                 }
 
-                if (j != i && j != c1) {
+                if (j != c1) {
                     *(dist_matrix_element(out, k, l)) = dist_matrix_distance(dmat, i, j);
                 }
 
