b1a3617fe0b5296ebaa39ee25945754523ceb2f3
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2018-03-19 16:20:22 +0100

    Changed name function and added the function btree_storage_fetch

--- src/phylogenetic_tree.c
+++ src/phylogenetic_tree.c
@@ -1,23 +1,31 @@
 #include "phylogenetic_tree.h"
-#include <stdlib.h>
 #include "utilities.h"
 
-btree *btree_size(uint32_t nodes_count) {
-    size_t struct_size = sizeof(btree) + (nodes_count * member_size(btree, nodes[0]));
+#include <assert.h>
+#include <stdlib.h>
+
+size_t btree_storage_size(uint32_t nodes_count) {
+    return sizeof(btree_storage) + (nodes_count * member_size(btree_storage, nodes[0]));
+}
+
+btree_storage *btree_storage_init(uint32_t nodes_count) {
+    size_t size = btree_storage_size(nodes_count);
+    btree_storage *storage = malloc(size);
+
+    return storage;
+}
+
+void btree_storage_free(btree_storage *storage) {
+    free(storage);
+}
+
+btree_node *btree_storage_fetch(btree_storage *storage) {
+    assert(storage->used_nodes < storage->nodes_count);
     
-    return struct_size;
+    uint32_t idx = storage->used_nodes;
+    btree_node *node = &storage->nodes[idx];
+    
+    storage->used_nodes++;
+    
+    return node;
 }
-
-btree *btree_init(uint32_t nodes_count) {
-    btree *btree;
-    btree = malloc(btree_size(nodes_count));
-
-    return btree;
-}
-
-void btree_free(btree *btree) {
-    if (btree) {
-        free(btree);
-    }
-}
-
--- src/phylogenetic_tree.h
+++ src/phylogenetic_tree.h
@@ -14,14 +14,24 @@ struct btree_node {
     char *node_name;
 };
 
-typedef struct btree btree;
+typedef struct btree_storage btree_storage;
 
-struct btree {
+struct btree_storage {
     /* Number of nodes */
     uint32_t nodes_count;
+    
+    /* Number of used nodes / Index of first available node */
+    uint32_t used_nodes;
     
     /* Array of tree nodes */
     btree_node nodes[];
 };
+
+size_t btree_storage_size(uint32_t nodes_count);
+btree_storage *btree_storage_init(uint32_t nodes_count);
+void btree_storage_free(btree_storage *tree);
+
+/* Fetch the first available node */
+btree_node *btree_storage_fetch(btree_storage *storage);
 
 #endif /* NEIGH_PHYLOGENETIC_TREE_H */
